10bf2241d6f6124abdba69e17cf62ae4763cfaac
pankaj singh <pankajsingh8540@gmail.com>
2022-12-22 07:12:00 +0530

    first commit

---
+++ backend/config/env.js
@@ -0,0 +1,11 @@
+module.exports = {
+    database: "exit_test",
+    username: "root",
+    password: "root1234",
+    // host: "localhost",
+    host:"db",
+    dialect: "mysql"
+};
+
+
+
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  // apiUrl: 'https://backend3211.azurewebsites.net/'
+apiUrl:'http://localhost:5000'
+  // apiUrl: 'https://10.0.2.4/'
+
+
+};
+// 
